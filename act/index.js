c61dc57323debcf8db69cb84343833d8aa870961
codetyphon <codetyphon@sina.com>
2015-01-27 17:24:47 +0800

    控制器可以查询数据

--- act/index.js
+++ act/index.js
@@ -1,5 +1,15 @@
-data=data.setValue({
-	'{title}':'title',
-	'{time}':'2015-01-25',
-	'{site}':'codetyphon'
+db_query("names",{tel:'54321'},function(d){
+	//
+	data=data.setValue({
+		'{title}':d[0].name,
+		'{time}':'2015-01-25',
+		'{site}':'codetyphon'
+	});
+	
+	console.log("d:"+d[0].name);
+	render();
+	//
+},function(err){
+	data=err;
+	render();
 });
